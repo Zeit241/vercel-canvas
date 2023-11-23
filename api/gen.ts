74bf96156dc3d15931d8ac6f54a277918e6436e1
Zeit241 <easywin.ru@gmail.com>
2023-11-23 18:44:57 +0300

    update

--- api/gen.ts
+++ api/gen.ts
@@ -1,10 +1,11 @@
 import { createCanvas } from  'canvas'
+import { NowRequest, NowResponse } from '@now/node'
 
-export default async function Generate(){
-    // const canvas = createCanvas(200, 200)
-    // const ctx = canvas.getContext('2d')
-    // ctx.font = '30px Impact'
-    // ctx.fillText('Awesome!', 50, 100)
-    // return canvas.toBuffer();
-    return 111
+export default (req: NowRequest, res: NowResponse) => {
+    const canvas = createCanvas(200, 200)
+    const ctx = canvas.getContext('2d')
+    ctx.font = '30px Impact'
+    ctx.fillText('Awesome!', 50, 100)
+    res.send(canvas.toBuffer() )
+    // return res.json({ name: 'John', email: 'john@example.com', buffer: });
 }
