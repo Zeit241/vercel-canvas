e4d19511495311b35e73dffc2d33ca1e72686e75
Zeit241 <easywin.ru@gmail.com>
2023-11-23 18:38:35 +0300

    update

--- api/gen.ts
+++ api/gen.ts
@@ -1,9 +1,10 @@
 import { createCanvas } from  'canvas'
 
 export default async function Generate(){
-    const canvas = createCanvas(200, 200)
-    const ctx = canvas.getContext('2d')
-    ctx.font = '30px Impact'
-    ctx.fillText('Awesome!', 50, 100)
-    return canvas.toBuffer();
+    // const canvas = createCanvas(200, 200)
+    // const ctx = canvas.getContext('2d')
+    // ctx.font = '30px Impact'
+    // ctx.fillText('Awesome!', 50, 100)
+    // return canvas.toBuffer();
+    return 111
 }
